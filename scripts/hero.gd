c84e6de771f77be56b93df361d55e3c96eed884b
hammoh7 <soahammohaadkar@gmail.com>
2024-05-25 23:42:11 +0530

    Hero

--- scripts/hero.gd
+++ scripts/hero.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -280.0
+@onready var animated_sprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,12 +15,28 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	# Input directions: -1, 0, 1
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	# Flips the direction of hero
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	# Animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("hero")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+	# Apply movements
 	if direction:
 		velocity.x = direction * SPEED
 	else:
